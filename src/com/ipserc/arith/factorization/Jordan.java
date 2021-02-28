e70fb2b071ddece8c57d92adb9ce38024fb51b29
ipserc <ipserc@users.noreply.github.com>
2021-02-28 19:50:35 +0100

    /src/com/ipserc/arith is the home of the souce

---
+++ src/com/ipserc/arith/factorization/Jordan.java
@@ -0,0 +1,158 @@
+package com.ipserc.arith.factorization;
+
+import com.ipserc.arith.matrixcomplex.Eigenspace;
+import com.ipserc.arith.matrixcomplex.MatrixComplex;
+import com.ipserc.arith.complex.Complex;;
+
+public class Jordan extends Eigenspace {
+
+	private final static String HEADINFO = "Jordan --- INFO:";
+	private final static String VERSION = "1.0 (2020_0627_1130)";
+
+	private MatrixComplex cJ;
+	private MatrixComplex cP;
+	private boolean factorized = false;
+
+	/*
+	 * 	CONSTRUCTORS 
+	 */
+	/**
+	 * Instantiates a complex square array of length len.
+	 * @param len The length of the square array.
+	 */
+	/*
+	public Jordan(int len) {
+		super(len);
+	}
+	*/
+	
+	/**
+	 * Instantiates a complex array from a string, rows are separated with ";", cols are separated with ",".
+	 * @param strMatrix the string with the rows and columns.
+	 */
+	public Jordan(String strMatrix) {
+		super(strMatrix);
+	}
+
+	/**
+	 * Instantiates a Diagfactor array from a MatrixComplex.
+	 * @param matrix the MatrixComplex already instantiated.
+	 */
+	public Jordan(MatrixComplex matrix) {
+		super(matrix);
+		//this.complexMatrix = matrix.complexMatrix.clone();
+	}
+
+	/*
+	 * 	GETTERS 
+	 */
+
+	/**
+	 * Gets the diagonal matrix
+	 * @return J
+	 */
+	public MatrixComplex J() {
+		return cJ;
+	}
+
+	/**
+	 * Gets the eigenvector matrix
+	 * @return P
+	 */
+	public MatrixComplex P() {
+		return cP;
+	}
+
+	/**
+	 * Gets the class member variable with the status of the factorization.
+	 * @return The factorization status.
+	 */
+	public boolean factorized() {
+		return factorized;
+	}
+
+	/**
+	 * 
+	 * @param order
+	 * @param arithMult
+	 * @param eigenValue
+	 * @return
+	 */
+	private MatrixComplex block(int order, int arithMult, Complex eigenValue) {
+		MatrixComplex block = new MatrixComplex(this.rows(), this.cols());
+		block.initMatrix(0, 0);
+		for (int i = 0, row = order; i < arithMult; ++i, ++row) {
+			block.setItem(row, row, eigenValue);
+			//if ( i+row > 0 && row+1 < this.cols()) block.setItem(row, row+1, Complex.ONE);
+			if ( row+1 < this.cols()) block.setItem(row, row+1, Complex.ONE);
+		}
+		return block;
+	}
+
+	/**
+	 * Calculates the eigenvector or characteristic vector using as generating space (M-lI)^a 
+	 * where M is the coef matrix, l is the eigenvalue, I is the identity matrix and a i the power to raise the  
+	 * @param eigenval The eigenvalue to calculate the eigenvector
+	 * @param arithMult The arithmetic multiplicity
+	 * @return A MatrixComplex with the eigenvectors
+	 */
+	public MatrixComplex eigenvectors(Complex eigenval, int arithMult) {
+		int rowLen = this.rows();
+		int colLen = this.cols();
+
+		MatrixComplex I = new MatrixComplex(rowLen, colLen); I.initMatrixDiag(1,0);
+		MatrixComplex eigenVectors = new MatrixComplex(rowLen, colLen);
+		MatrixComplex eigenVect;
+
+		MatrixComplex cMatrix = (this.minus(I.times(eigenval))).power(arithMult);
+		MatrixComplex dMatrix = cMatrix.augment().heap();
+		eigenVect = dMatrix.solve();
+		return eigenVect;
+		/*
+		for (int sol = 0; sol < eigenVect.rows(); ++sol)
+		//for (int sol = 0; sol < arithMult; ++sol)
+			eigenVectors.complexMatrix[sol] = eigenVect.complexMatrix[sol].clone();
+		return eigenVectors;
+		*/
+	}
+
+	/**
+	 * Factorizes the matrix using a diagonal matrix of eigenvectors (D) and a eigenvalue matrix (P)
+	 * The factorization gives A=P·J·P⁻¹
+	 */
+	public void factorize() {
+		int rowLen = this.rows(); 
+		int colLen= this.cols();
+		if (colLen != rowLen) {
+			System.out.println(HEADINFO + "The Matrix MUST be square to be factorized as a Jordan Matrix");
+			System.exit(-1);
+		}
+		MatrixComplex eigenValArray = this.values();
+		eigenValArray.quicksort(0);
+		
+		cJ = new MatrixComplex(rowLen, colLen);
+		cP = new MatrixComplex(rowLen, colLen);
+
+		for (int i = 0; i < eigenValArray.rows();) {
+			Complex eigenval = eigenValArray.getItem(i, 0);
+			int arithMult = this.arithmeticMultiplicity(eigenval);
+			MatrixComplex jordanBlock = this.block(i, arithMult, eigenval);
+			cJ = cJ.plus(jordanBlock);
+			i += arithMult;
+		}
+		cJ.println("----------JORDAN");
+		
+		for (int i = 0; i < eigenValArray.rows();) {
+			Complex eigenval = eigenValArray.getItem(i, 0);
+			int arithMult = this.arithmeticMultiplicity(eigenval);
+			MatrixComplex eigenVect = this.eigenvectors(eigenval, arithMult);
+			eigenVect.println("----------EIGENVECTORS for:" + eigenval.toString() + " multiplicity:" + arithMult);
+			for (int sol = 0; sol < eigenVect.rows(); ++sol) {
+				cP.complexMatrix[sol+i] = eigenVect.complexMatrix[sol].clone();
+				
+			}
+			i += arithMult;
+		}
+		cP.println("----------PASS MATRIX");
+	}
+}
---
+++ src/com/ipserc/arith/syseq/Syseq.java
@@ -0,0 +1,264 @@
+package com.ipserc.arith.syseq;
+
+import com.ipserc.arith.matrixcomplex.MatrixComplex;
+import com.ipserc.arith.complex.*;
+
+public class Syseq extends MatrixComplex{
+	private MatrixComplex partsol;	// Solution for the system
+	private MatrixComplex homosol;	// Solution for the homogeneous system
+	private Boolean solved = false;
+
+	private final static String HEADINFO = "Syseq --- INFO: ";
+	private final static String VERSION = "1.1 (2021_0207_2100)";
+
+	/*
+	 * ***********************************************
+	 * 	VERSION 
+	 * ***********************************************
+	 */
+	
+	/**
+	 * Prints Class Version
+	 */
+	public static void version() {
+		System.out.println(HEADINFO + "VERSION:" + VERSION); 
+	}
+	
+	/*
+	 * ***********************************************
+	 * 	CONSTRUCTORS 
+	 * ***********************************************
+	 */
+
+	/**
+	 * Instantiates an empty system of equations
+	 */
+	public Syseq() {
+		super();
+	}
+
+	/**
+	 * Instantiates the system of equations of dimensions rowLen x rowLen + 1.
+	 * @param rowLen Number of rows.
+	 */	
+	public Syseq(int rowLen) {
+		 super(rowLen, rowLen +1);
+	}
+	
+	/**
+	 * Instantiates the system of equations given by the string "cadena" as a complex matrix representation
+	 * @param cadena the matrix with the equation system values
+	 */
+	public Syseq(String cadena) {
+		super(cadena);
+	}
+	
+	/**
+	 * Instantiates the system of equations given matrix as a complex matrix representation
+	 * @param matrix The matrix with the equation system values
+	 */
+	public Syseq(MatrixComplex matrix) {
+		this.complexMatrix = matrix.complexMatrix;
+	}
+
+	/*
+	 * ***********************************************
+	 * OPERATIONS
+	 * ***********************************************
+	 */
+
+	/**
+	 * Solves the equation system as a linear combination of a particular solution and the solution of the homogeneous system
+	 * @param lambda The complex value to calculate the solutions
+	 */
+	public void solveq(Complex lambda) {
+		partsol = this.solve(lambda);
+		if (this.typeEqSys() == INDETERMINATE) {
+			homosol = this.homogeneous().solve(lambda);
+		}
+		solved = true;
+	}
+
+	/**
+	 * Solves the equation system as a linear combination of a particular solution and the solution of the homogeneous system with lamda = 1
+	 */
+	public void solveq() {
+		this.solveq(Complex.ONE);
+	}
+
+	/**
+	 * Returns the corresponding solution as [particular sol]+n*[homogeneous sol]
+	 * @param n The value of the n 
+	 * @return The solution calculated
+	 */
+	public MatrixComplex solution(double n) {
+		MatrixComplex sol = new MatrixComplex();
+		
+		if (!solved) this.solveq();
+		switch (this.typeEqSys()) {
+			case DETERMINATE: sol = partsol; break;
+			case INDETERMINATE: sol =  partsol.plus(homosol.times(n)); break;
+			case INCONSISTENT: sol = partsol; break;
+		}
+		return sol;
+	}
+	
+	/**
+	 * Returns the corresponding solution as [particular sol]+[homogeneous sol]
+	 * @return The solution calculated
+	 */
+	public MatrixComplex solution() {
+		return this.solution(1);
+	}
+	
+	/*
+	 * ***********************************************
+	 * PRINTING
+	 * ***********************************************
+	 */
+
+	/**
+	 * Prints the equation system as N(0) x0 + N(1) x1 + ... N(n) xn = N(n+1)
+	 * @param Title The title
+	 */
+	public void print(String Title) {
+		System.out.println(Title);
+		for (int row = 0; row < rows(); ++ row) {
+			int col = 0;
+			for (; col < cols() - 1; ++col) {
+				System.out.print("+("+getItem(row, col)+")x"+col);
+			}
+			System.out.println(" = "+getItem(row, col));
+		}
+		printTypeEqSys();
+	}
+
+	/**
+	 * Prints the equation system as N(0) x0 + N(1) x1 + ... N(n) xn = N(n+1)
+	 */
+	public void print() {
+		this.print("");
+	}
+	
+	/**
+	 * Prints the particular and homogeneous solutions if exists
+	 */
+	public void printSol(String title) {
+		System.out.println(title);
+		if (!solved) this.solveq();
+		if (typeEqSys() == INCONSISTENT) {
+			System.out.println("There are no solutions for an INCONSISTENT Equation System");
+		}
+		else {
+			partsol.println("Particular  Solution");
+			if (homosol != null) homosol.println("Homogeneous Solution");
+		}
+	}
+
+	/**
+	 * Returns the "new Syseq" instruction for the matrix
+	 */
+	public String MatrixComplex_sysEq() {
+		String toMMatrixComplex;
+		toMMatrixComplex = this.preMatrixComplex();
+		toMMatrixComplex = "Syseq aMatrix = new Syseq("+");";
+		return toMMatrixComplex;
+	}
+	
+	/**
+	 * Returns the linsolve command for Maxima with the equations and the unknowns blocks. It can use expand if enabled.
+	 * @param expand if True adds expand to the linsolve
+	 * @return The linsolve command for Maxima
+	 */
+	public String toMaxima_linsolve(boolean expand) {
+		int row, col;
+		//Set up equations block
+		String toWolfram = "([";
+		for (row = 0; row < rows(); ++ row) {
+			for (col = 0; col < cols()-1; ++col) {
+				toWolfram = toWolfram + "+("+getItem(row, col)+")*x"+col;
+			}
+			toWolfram = toWolfram + "="+getItem(row, col);
+			toWolfram = toWolfram + (row < rows()-1 ? "," : "],[") ;
+		}
+		// replace "i" with "*%i" (imaginary term for Maxima)
+		toWolfram = toWolfram.replace("i", "*%i");
+		
+		// Set up unknowns block
+		for (col = 0; col < cols() - 2; ++col) {
+			toWolfram = toWolfram + "x"+col+",";
+		}
+		toWolfram = toWolfram + "x"+col+"])";
+		
+		// Header for linsolve with expand
+		toWolfram = "linsolve" + toWolfram;
+		if (expand) toWolfram = "expand("+ toWolfram + ")";
+
+		return toWolfram;
+	}
+	
+	/**
+	 * Returns the linsolve command for GNU Octave with the equations and the unknowns blocks. It can use disp if enabled.
+	 * @param disp if True adds disp to the linsolve
+	 * @return The linsolve command for GNU Octave
+	 */
+	public String toOctave_linsolve(boolean disp) {
+		String toOctave = "Sol = linsolve(";
+		MatrixComplex A, B;
+		A = this.coefMatrix();
+		B = this.constMatrix();
+		toOctave += (A.toOctave()+","+B.toOctave()+")");
+		if (disp) toOctave = "disp(" + toOctave + ")";
+		return toOctave;
+	}
+	
+	/**
+	 * Returns the linsolve command for Matlab with the equations and the unknowns blocks. It can use disp if enabled.
+	 * @param disp if True adds disp to the linsolve
+	 * @return The linsolve command for Matlab
+	 */
+	public String toMatlab_linsolve(boolean disp) {
+		return toOctave_linsolve(disp);
+	}
+	
+	/**
+	 * Returns the solve command for Wolfram with the equations and the unknowns blocks.
+	 * @return The solve command for Wolfram
+	 */
+	public String toWolfram_solve() {
+		int row, col;
+		//Set up equations block
+		String toWolfram = "solve(";
+		for (row = 0; row < rows(); ++ row) {
+			for (col = 0; col < cols()-1; ++col) {
+				toWolfram = toWolfram + "+("+getItem(row, col)+")*x"+col;
+			}
+			toWolfram = toWolfram + "="+getItem(row, col);
+			toWolfram = toWolfram + (row < rows()-1 ? "," : ",") ;
+		}
+		
+		// Set up unknowns block
+		for (col = 0; col < cols() - 2; ++col) {
+			toWolfram = toWolfram + "x"+col+",";
+		}
+		toWolfram = toWolfram + "x"+col+")";
+
+		return toWolfram;
+	}
+
+	/**
+	 * Prints the system equations solve command for MATRIXCOMPLEX, MAXIMA, OCTAVE, MATLAB, WOLFRAM
+	 * @param format Onf the allowed fprmats (MATRIXCOMPLEX, MAXIMA, OCTAVE, MATLAB, WOLFRAM)
+	 * @param display If true adds "disp" or "expand" to the solve instruction if needed
+	 */
+	public void printSystemEqSolve(outputFormat format, boolean display) {
+		switch (format) {
+			case MATRIXCOMPLEX: System.out.println("MATRIXCOMPLEX:"+MatrixComplex_sysEq()); break;
+			case MAXIMA: System.out.println("MAXIMA:"+toMaxima_linsolve(display)); break;
+			case OCTAVE: System.out.println("OCTAVE:"+toOctave_linsolve(display)); break;
+			case MATLAB: System.out.println("MATLAB:"+toMatlab_linsolve(display)); break;
+			case WOLFRAM: System.out.println("WOLFRAM:"+toWolfram_solve()); break;				
+		}
+		
+	}
+}
