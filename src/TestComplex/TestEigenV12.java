4b80610dd4fe7932bc9c050d22850369b6e5fa41
ipserc <ipserc@users.noreply.github.com>
2021-11-04 20:55:50 +0100

    Tests renamed and corrected according the new changes

---
+++ src/TestComplex/TestEigenV12.java
@@ -0,0 +1,177 @@
+package TestComplex;
+
+import com.ipserc.arith.matrixcomplex.*;
+import com.ipserc.arith.matrixcomplex.MatrixComplex.outputFormat;
+import com.ipserc.arith.complex.*;
+import com.ipserc.arith.factorization.Diagfactor;
+
+public class TestEigenV12 {
+
+	public static void doEigenCalculations(MatrixComplex aMatrix) {
+		Complex seed = new Complex(1,0);
+		Eigenspace eigenSpace = new Eigenspace(seed, aMatrix);
+     	MatrixComplex eigenVect;
+     	int boxSize = 65;
+
+       	System.out.println("");
+       	System.out.println(Complex.boxTitleRandom(boxSize, "EIGENVALUES & EIGENVECTORS TEST"));
+     	Complex.facts();
+    	aMatrix.println("aMatrix");
+    	
+    	System.out.println("aMatrx condition:" + aMatrix.cond());
+
+    	System.out.println(Complex.boxTextRandom(boxSize, "EigenVectors Expressions"));
+    	System.out.println("Maxima:"+eigenSpace.Maxima_eigenvalues(true));
+    	System.out.println("Maxima:"+eigenSpace.Maxima_eigenvectors(true));
+    	System.out.println("Maxima:"+eigenSpace.Maxima_charpoly(true));
+    	System.out.println("Octave:"+eigenSpace.Octave_eigenvectors());
+    	System.out.println("Wolfram:"+eigenSpace.Wolfram_eigenvectors());
+
+    	System.out.println(Complex.boxTextRandom(boxSize, "Determinant, Triangle & Characteristic polynomial"));
+    	aMatrix.determinant().println("Determinant:");
+    	aMatrix.triangle().heap().println("triangle:");
+    	eigenSpace.getCharactPoly().println("Characteristic polynom:");
+
+    	System.out.println(Complex.boxTextRandom(boxSize, "EigenValues Calculated"));
+    	{
+    		int numDecs = (int)(aMatrix.cond()/2);
+    		numDecs = numDecs > Complex.getSignificative() ? Complex.getSignificative() : numDecs;
+
+	    	Complex eVal = eigenSpace.values().getItem(0,0);
+			System.out.println("EigenValue: " + eVal.toString() + 
+					" - arith mult:" + eigenSpace.arithmeticMultiplicity(eVal) + 
+					" - geom mult:" + eigenSpace.geometricMultiplicity(eVal));    		
+	    	for (int i = 0; i < eigenSpace.values().rows(); ++i) {
+	    		if (eVal.equalsred(eigenSpace.values().getItem(i,0), numDecs)) continue;
+	    		eVal = eigenSpace.values().getItem(i,0);
+				System.out.println("EigenValue: " + eVal.toString() + 
+						" - arith mult:" + eigenSpace.arithmeticMultiplicity(eVal) + 
+						" - geom mult:" + eigenSpace.geometricMultiplicity(eVal));    		
+	    	}
+    	}
+
+    	System.out.println(Complex.boxTextRandom(boxSize, "Characteristics Equations"));
+    	System.out.println(Complex.boxTextRandom(boxSize, "OCTAVE"));
+    	eigenSpace.printCharactEq(outputFormat.OCTAVE, true);
+       	System.out.println(Complex.boxTextRandom(boxSize, "MAXIMA"));
+    	eigenSpace.printCharactEq(outputFormat.MAXIMA, true);
+       	System.out.println(Complex.boxTextRandom(boxSize, "MATRIXCOMPLEX"));
+    	eigenSpace.printCharactEq(outputFormat.MATRIXCOMPLEX, true);
+ 
+    	eigenSpace.vectors().println(Complex.boxTextRandom(boxSize, "EigenVectors Calculated"));
+    	System.out.println("EigenVectors - Determinant:"+eigenSpace.vectors().determinant());
+   	
+    	System.out.println(Complex.boxTextRandom(boxSize, "Check eigenvectors"));
+    	int colLen = aMatrix.cols(); //complexMatrix[0].length;
+    	eigenVect = new MatrixComplex(1,colLen);
+    	
+    	for (int eigv = 0; eigv < eigenSpace.values().rows(); ++eigv) {
+    		eigenVect.complexMatrix[0] = eigenSpace.vectors().complexMatrix[eigv].clone();
+    		Complex eigenVal = eigenSpace.values().complexMatrix[eigv][0];
+	    	//eigenVect.divides(eigenVect.complexMatrix[0][0]).println("Norm eigenVect "+eigv);
+	    	eigenVect.println ("**************** eigenVect:");
+	    	System.out.println("                 eigenVal : "+ eigenVal.toString());
+	    	aMatrix.times(eigenVect.transpose()).transpose().println("aMatrix·eigenVect  "+eigv);
+	    	eigenVect.times(eigenVal).println("eigval["+eigv+"]·eigenVect"+eigv);
+    	}
+
+    	System.out.println(Complex.boxTextRandom(boxSize, "Some other calculations"));
+    	eigenSpace.vectors().adjoint().times(eigenSpace.vectors()).println("eVT·eV:");
+    	Diagfactor diagonal = new Diagfactor(aMatrix);
+    	if (eigenSpace.isDiagonaizable()) {
+        	System.out.println(Complex.boxTextRandom(boxSize, "IS DIAGONALIZABLE"));
+        	diagonal.diagonalize();
+        	if (diagonal.factorized()) {
+    	    	diagonal.D().println("Matriz Diagonal (D):");
+    	    	diagonal.P().println("Matriz Valores Propios (P):");
+    	    	diagonal.P().times(diagonal.D()).times(diagonal.P().inverse()).println("P·D·P⁻¹:");
+        	}
+    	}
+    	else {
+        	System.out.println(Complex.boxTextRandom(boxSize, "IS NOT!!!!!!!!!!!!!! DIAGONALIZABLE"));    		
+    	}
+    	/*
+    	System.out.print("press any key");
+    	try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		*/
+       	System.out.println(Complex.boxTextRandom(boxSize, "-- END EIGENVALUES & EIGENVECTORS TEST END --"));
+    	System.out.println();
+    	System.out.println();
+    	System.out.println();
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    	MatrixComplex aMatrix = new MatrixComplex();
+
+     	Complex.setFormatON();
+     	Complex.setFixedON(3);
+     	Eigenspace.version();
+
+     	/* */
+     	aMatrix = new MatrixComplex(""+
+     			"  7 ,  -3 ,  1 ;" +
+     			"  0 ,   7 , -3 ;" +
+     			"  0 ,   0 ,  7 ");
+     	doEigenCalculations(aMatrix);
+
+     	aMatrix = new MatrixComplex(""+
+     			" -4 ,  -3 ,  1 ;" +
+     			"  0 ,  -4 , -3 ;" +
+     			"  0 ,   0 , -4 ");
+     	doEigenCalculations(aMatrix);
+
+     	aMatrix = new MatrixComplex(""+
+     			"-11 ,  -3 ,  1 ;" +
+     			"  0 , -11 , -3 ;" +
+     			"  0 ,   0 ,-11 ");
+     	doEigenCalculations(aMatrix);
+
+     	aMatrix = new MatrixComplex(""+
+     			" 11 ,  -3 ,  1 ;" +
+     			"  0 ,  11 , -3 ;" +
+     			"  0 ,   0 , 11 ");
+     	doEigenCalculations(aMatrix);
+
+     	aMatrix = new MatrixComplex(""+
+     			" 11 ,   5 ,   1 ;" +
+     			"  0 ,  11 , -3 ;" +
+     			"  0 ,   0 , 11 ");
+     	doEigenCalculations(aMatrix);
+     	
+     	aMatrix = new MatrixComplex(""+
+     			" 13 ,  8 ,  8 ;" +
+     			" -1 ,  7 , -2 ;" +
+     			" -1 , -2 ,  7 ");
+
+     	doEigenCalculations(aMatrix);
+
+     	Complex.significative(3);
+     	doEigenCalculations(aMatrix);
+     	
+     	Complex.precision(1e-10);
+     	doEigenCalculations(aMatrix);
+
+     	Complex.precision(1e-8);
+     	Complex.zero_threshold_r(1e-5);
+     	doEigenCalculations(aMatrix);
+
+     	Complex.precision(1e-4);
+     	doEigenCalculations(aMatrix);
+     	
+     	aMatrix = new MatrixComplex("1.000,2.000,3.000;2.000,4.000,6.000;3.000,6.000,9.000");
+     	doEigenCalculations(aMatrix);
+     	
+     	aMatrix = new MatrixComplex("1,2,3,-i;0,+2i,-1,2;3i,0,1,1;4,-2,-i,5");
+     	doEigenCalculations(aMatrix);
+     	
+     	aMatrix = new MatrixComplex("1,2,3;0,-1,2;0,1,1");
+     	doEigenCalculations(aMatrix);
+	}
+}
---
+++ src/TestComplex/plotFunc04.java
@@ -0,0 +1,94 @@
+package TestComplex;
+
+import java.util.function.Function;
+import com.ipserc.arith.complex.*;
+import com.ipserc.arith.matrixcomplex.MatrixComplex;
+import com.ipserc.arith.polynom.Polynom;
+import com.panayotis.gnuplot.JavaPlot;
+
+public class plotFunc04 {
+	/**
+	 * Plots the function expressed in GNUPlot format between loLimit and upLimit.
+	 * @param GNUplotExpression The function to plot in GNUPlot format.
+	 * @param loLimit Lower limit of the plot.
+	 * @param upLimit Upper limit of the plot.
+	 */
+	public static void plot(String title, long samples, double dataRe[][], double dataIm[][]) {
+		JavaPlot p = new JavaPlot();
+		p.setTitle(title);
+		p.addPlot(dataRe);
+		p.addPlot(dataIm);
+		p.set("zeroaxis", "");
+		p.set("style","data lines");
+		//p.set("xrange", "[0:"+samples+"]");
+		p.set("grid","");
+		p.plot();
+	}
+	
+	public static void plot(String title, long samples, MatrixComplex data) {
+		double dataRe[][]  = new double[(int)samples+1][1];
+		double dataIm[][]  = new double[(int)samples+1][1];
+
+		for (int t = 0; t <= samples; ++t) {
+			dataRe[t][0] = data.complexMatrix[t][0].rep();
+			dataIm[t][0] = data.complexMatrix[t][0].imp();
+		}
+		plot(title, samples, dataRe, dataIm);
+	}
+
+	public static void main(String[] args) {
+		int decPrec = 3;
+		Complex.setScientificON(decPrec);
+		//String dashes = "-".repeat(80);
+		Complex lolimit = new Complex();
+		Complex uplimit = new Complex();
+		Complex point = new Complex();
+		Complex point1 = new Complex();
+		Complex point2 = new Complex();
+		Complex midpoint = new Complex();
+		Complex integral = new Complex();
+		double stepRe;
+		double stepIm;
+		Function<Complex, Complex> func;
+		func = z -> z.times(Complex.sin(z)).divides(z.mod());
+		
+		int samples = 300;
+		MatrixComplex data = new MatrixComplex(samples+1, 1);
+		lolimit.setComplex("-1.9+1i");
+		uplimit.setComplex("+1-1.9i");
+		stepRe = uplimit.minus(lolimit).rep()/samples;
+		stepIm = uplimit.minus(lolimit).imp()/samples;
+		
+		point1 = lolimit;
+		point2.setComplexRec(point1.rep() + stepRe, point1.imp() + stepIm);
+		Complex cte = new Complex(26,-35);
+		integral = point1.times(2).plus(cte);
+		for (int t = 0; t <= samples; ++t) {
+			integral = integral.plus(Complex.integrate(point1, point2, func, decPrec));
+			data.complexMatrix[t][0] = integral.copy();
+			//data.complexMatrix[t][0] = Complex.integrate(point1, point2, func, decPrec);
+			System.out.println("point1:" + point1.toString() + "  point2:" + point2.toString());
+			System.out.println("step:" + t + " Data:" + data.complexMatrix[t][0].toString());
+			midpoint.setComplexRec(point1.rep()+stepRe/2, point1.imp()+stepIm/2);
+			System.out.println("CHECK midpoint:" + midpoint.toString() + " Valor:" + midpoint.power(2).toString());
+			point1 = point2.copy();
+			point2.setComplexRec(point1.rep() + stepRe, point1.imp() + stepIm);
+		}
+		String title = "Integral func = z*sin(z)/|z| (" + lolimit.toString() + "," + uplimit.toString()+")";
+		plot(title, samples, data);
+
+		point1 = lolimit;
+		point = point1.copy();
+		for (int t = 0; t <= samples; ++t) {			
+			data.complexMatrix[t][0] = func.apply(point);
+			System.out.println("point:" + point.toString());			
+			System.out.println("step:" + t + " Data:" + data.complexMatrix[t][0].toString());
+			point.setComplexRec(point1.rep() + stepRe/2, point1.imp() + stepIm/2);
+			point1.setComplexRec(point1.rep() + stepRe, point1.imp() + stepIm);
+			
+		}
+		title = "func = z*sin(z)/|z| (" + lolimit.toString() + "," + uplimit.toString()+")";
+		plot(title, samples, data);
+	}
+
+}
