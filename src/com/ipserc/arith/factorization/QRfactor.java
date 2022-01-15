1b422b41548826f2adb1f1ad5872c61ce6083de8
ipserc <ipserc@users.noreply.github.com>
2022-01-15 11:05:36 +0100

    1.0 (2020_0824_1800)

--- src/com/ipserc/arith/factorization/QRfactor.java
+++ src/com/ipserc/arith/factorization/QRfactor.java
@@ -30,6 +30,11 @@ public class QRfactor extends MatrixComplex {
 
 	private final static String HEADINFO = "QRfactor --- INFO: ";
 	private final static String VERSION = "1.0 (2020_0824_1800)";
+	/* VERSION Release Note
+	 * 
+	 * 1.0 (2020_0824_1800)
+	 */
+
 
 	/*
 	 * ***********************************************
